bed5a527aafa482ac2f86e92b341b173d3af90c6
Jacob Erceg <71945422+Jacob-M-Erceg@users.noreply.github.com>
2022-08-02 18:50:15 -0700

    Initial Commit

---
+++ TodoPad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoPad
+//
+//  Created by John Lee on 2022-08-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
